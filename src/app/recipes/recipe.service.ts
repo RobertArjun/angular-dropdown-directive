398d14559fc449b1a11e7efaf2a8464c11c9f6d4
RobertArjun <robertarjun46@gmail.com>
2019-12-26 12:13:25 +0530

    1 file modified

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,7 +11,7 @@ export class RecipeService {
     new Recipe('Tantoori Chicken','Tantoori Kebap','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSvHia79vbWB9UaN7MjxEulucFfMGdYh_0WC4Sh0z7vHgSojnYy')
   ]
 
-  recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>(); // Provide exact copy of Array
 
   constructor() { }
 
